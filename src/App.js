183e8f22c1bd16006b53e91d5d451516b33bf049
mukesh.rai <mukesh.rai@amniltech.com>
2023-03-19 00:44:08 +0545

    #Feature Counter App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import Button from "./Button";
+import "./css/Counter.css";
+
 function App() {
-    return (
-        <div>
-            <h1>Hello React</h1>
-            <h3>Welcome to React Learning Phase</h3>
-            <Button name="Google" link="https://Google.com" />
-            <Button name="Facebook" link="https://Facebook.com" />
-            <Button name="Youtube" link="https://Youtube.com" />
-            <Button name="twitter" link="https://twitter.com" />
-            <Button name="Github" link="https://github.com" />
-        </div>
-    );
+  const [number, setNumber] = useState(0);
+  return (
+    <div>
+      <div>
+        <h1>Hello React</h1>
+        <h3>Welcome to React Learning Phase</h3>
+        <Button name="Google" link="https://Google.com" />
+        <Button name="Facebook" link="https://Facebook.com" />
+        <Button name="Youtube" link="https://Youtube.com" />
+        <Button name="twitter" link="https://twitter.com" />
+        <Button name="Github" link="https://github.com" />
+      </div>
+      <div>
+        <p className="result">{number}</p>
+        <button onClick={()=>setNumber(number +1)} className="counter-button">Add</button>
+        <button onClick={()=>setNumber(number-1)} className="counter-button">Less</button>
+        <button onClick={()=>setNumber(0)} className="counter-button">Reset</button>
+      </div>
+    </div>
+  );
 }
 // const App_ES6 = () => {
 //   return (
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./style.css";
+// import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
+
+import "./css/style.css";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// import React from "react";
-// import createRoot from "react-dom/client";
-
-// import "./style.css";
-// import App from "./App";
-
-// const container = document.getElementById("root");
-// const root = createRoot(container); // createRoot(container!) if you use TypeScript
-// root.render(<App tab="home" />);
+// Old Way 
+// ReactDOM.render(<App />, document.getElementById("root"));
+// After React 18
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
